e51ea9ea2979ddbd7a94d2f0d860a0edbe119ba6
GZHY <gaoz_y@163.com>
2017-03-04 15:01:34 +0800

    + 只对某个网页元素截图保存 + 重构html2image.py

--- html2image/html2image.py
+++ html2image/html2image.py
@@ -8,30 +8,68 @@ __author__ = 'GZhY'
 __version__ = 1.0
 
 from selenium import webdriver
+from PIL import Image
+import io
 import time
 import JSInjection
 
 
-def getImage(url, jsCode=None, saveImageName="result.png"):
-    browser = webdriver.PhantomJS()
-    browser.set_window_size(1200, 900)
-    browser.get(url)
-    if jsCode is not None and isinstance(jsCode, JSInjection.JSCode) and jsCode.getJSCode != "":
-        print(jsCode.getJSCode())
-        browser.execute_script(jsCode.getJSCode())
-        for i in range(30):
-            print(browser.title)
-            if jsCode.finishedSign in browser.title:
-                break
-            time.sleep(10)
+class Html2Image:
+    def __init__(self, url, jsCode=None):
+        self.browser = webdriver.PhantomJS()
+        self.browser.set_window_size(1200, 900)
+        self.browser.get(url)
+        self.jsCode = jsCode
+        self.image = None
 
-    browser.save_screenshot(saveImageName)
-    browser.close()
+    def getImage(self):
+        if self.jsCode is not None and isinstance(self.jsCode, JSInjection.JSCode) and self.jsCode.getJSCode != "":
+            # print(self.jsCode.getJSCode())
+            self.browser.execute_script(self.jsCode.getJSCode())
+            for i in range(30):
+                # print(self.browser.title)
+                if self.jsCode.finishedSign in self.browser.title:
+                    break
+                time.sleep(10)
 
+        self.image = self.browser.get_screenshot_as_png()
+        # self.browser.close()
+        return self.image
 
-def getElementImage():
-    pass
+    def getElementImage(self, cssSelector):
+        if not self.image:
+            self.getImage()
+        element = self.browser.find_element_by_css_selector(cssSelector)
+        left, top = element.location['x'], element.location['y']
+        right = left + element.size['width']
+        bottom = top + element.size['height']
+        im = Image.open(io.BytesIO(self.image))
+        im = im.crop((left, top, right, bottom))
+        # im.show()
+        imgByteArr = io.BytesIO()
+        im.save(imgByteArr, format='PNG')
+        return imgByteArr.getvalue()
+
+    def saveImage(self, image=None, filename="result.png"):
+        if not image:
+            if not self.image:
+                image = self.getImage()
+            else:
+                image = self.image
+        try:
+            with open(filename, 'wb') as f:
+                f.write(image)
+        except IOError:
+            return False
+        finally:
+            image = None
+        return True
+
+    def __del__(self):
+        self.browser.close()
 
 
 if __name__ == '__main__':
-    getImage("http://www.baidu.com", JSInjection.Scroll2Bottom())
+    h2i = Html2Image("https://www.baidu.com/", JSInjection.Scroll2Bottom())
+    h2i.saveImage(h2i.getImage())
+    h2i.saveImage(h2i.getElementImage("#head > div > div.s_form > div"), "element.png")
